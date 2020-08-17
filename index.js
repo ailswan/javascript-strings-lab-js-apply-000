9e7287d6e21b95a04f9b7438151bdab17d11f4ff
Wei Shi <ailswanster@gmail.com>
2020-08-17 20:40:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let\'s talk about ".repeat(2) + "space" +".".repeat(2);
+var topic = "Let\'s talk about "+ "space.";
 
 var conversation = `${topic}`;
